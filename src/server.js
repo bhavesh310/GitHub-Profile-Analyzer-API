e76c8caffd433f791b281670752f2ecbecefeb4f
Bhavesh Ghatode <bhaveshghatode1@gmail.com>
2026-06-08 09:50:13 +0530

    fix root route

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "GitHub Profile Analyzer API is running 🚀"
+  });
+});
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`GitHub Profile Analyzer running on port ${port}`);
